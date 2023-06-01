eb151ff9b87733de83b83997fca1991af69027bb
MUTHUKRISHNAN S <77878932+MuthuKrishnan27@users.noreply.github.com>
2023-06-01 17:13:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let button = document.getElementsByTagName("input")[0];
+        button.addEventListener("click",selectAndRemove);
+        function selectAndRemove(){
+            let dropdown = document.getElementById("colorSelect");
+            let selectedIndex = dropdown.selectedIndex;
+            dropdown.options[selectedIndex].remove(); 
+        }
